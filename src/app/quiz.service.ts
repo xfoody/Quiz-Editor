067591d37da5118c8f6bbe5bc1cba294924f0979
Fuedti Xayyavong <35578738+xfoody@users.noreply.github.com>
2019-04-22 19:47:10 -0500

    Added httpheaders

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +18,29 @@ export class QuizService {
     //   , { name: 'Quiz 2', numberQuestions: 0 }
     // ];
   }
+
+  saveQuizzes(changedQuizzes: any[], newQuizzes: any[] = []) {
+
+  let h = new HttpHeaders({
+    'Content-Type': 'application/json'
+    , 'X-Sas-Token': 'sig=K2WE6NQPtyoV6ke5hwPEaEaW52fgvyFWUeCEdPJls1s'
+  });
+
+  //console.log(h);
+
+  return this.builtInAngularHttpClient.post(
+    'https://modern-js.azurewebsites.net/save-quizzes-proxy'
+    , JSON.stringify(
+      {
+        "changedQuizzes": changedQuizzes
+        , "newQuizzes": newQuizzes
+      }
+    )
+    , {
+      headers: h
+    }
+  );
+}
 
   getNumberPromise(doYouWantMeToSucceed: boolean): Promise<number> {
     let p = new Promise<number>(
